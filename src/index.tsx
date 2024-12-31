c078a8c152cfec1cade92cca3d011556a00cfcc4
Dima <auga3blue@mail.ru>
2024-12-31 17:46:10 +0300

    mishaTeam

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,15 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 import './styles/body.scss'
 import { App } from './components/app/App';
-
+import mishaImg from './images/misha.jpg'
+document.addEventListener('DOMContentLoaded', () => { // Ждем загрузки DOM
+    const body = document.querySelector('body');
+    if (body) {
+      body.style.backgroundImage = `url(${mishaImg})`;
+    } else {
+      console.error("Body element not found!");
+    }
+  });
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
