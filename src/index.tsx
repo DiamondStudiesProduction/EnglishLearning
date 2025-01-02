c7ff9da940d80ecd07fa8917f0d1ad9e84546387
Dima <auga3blue@mail.ru>
2025-01-03 02:58:29 +0300

    photo

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import './styles/index.scss';
 import './styles/body.scss';
 import { App } from './components/app/App';
 import girls from './images/girls.jpg';
+import background from './images/background.jpg';
 import { useState } from 'react';
 document.addEventListener('DOMContentLoaded', () => {
 	// Ждем загрузки DOM
 	const body = document.querySelector('body');
 
 	if (body) {
-		body.style.backgroundImage = `url(${girls})`;
+		body.style.backgroundImage = `url(${background})`;
 	} else {
 		console.error('Body element not found!');
 	}
