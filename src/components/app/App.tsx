e334a55fe359c1c5248c980c1d285ebf7f9a7679
Dima <auga3blue@mail.ru>
2025-01-04 14:00:32 +0300

    nov

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,10 +5,15 @@ export const App = () => {
 	return (
 		<>
 			<Helmet>
-				<link
-					rel='icon'
-					type='image/jpg'
-					href={flags}
+				<link rel='icon' type='image/jpg' href={flags} />
+				<meta
+					property='og:url'
+					content='https://diamondstudiesproduction.github.io/EnglishLearning/'
+				/>
+				<meta property='og:site_name' content='English Learning' />
+				<meta
+					property='og:image'
+					content='https://i.pinimg.com/originals/28/84/a2/2884a28141a35375d125ef70fc1f913c.jpg'
 				/>
 			</Helmet>
 			<Home />
